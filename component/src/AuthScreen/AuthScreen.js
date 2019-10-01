23e6b30113fffd7cb6b5a53f1dc1f909a081f9dc
dimesha <dimesha99kanchani@gmail.com>
2019-10-01 11:08:34 +0530

    Auth_Screen_create

--- component/src/AuthScreen/AuthScreen.js
+++ component/src/AuthScreen/AuthScreen.js
@@ -2,9 +2,6 @@ import React, { Component } from 'react';
 import { Platform, Alert, ImageBackground } from 'react-native';
 import { } from 'native-base';
 import styles from '../AuthScreen/AuthScreenStyle'
-// import styles from './HomeScreen.Style'
-// import Card from '../../components/base/Card';
-// import Button from '../../components/base/Button'
 export default class App extends Component {
     render () {
         return(
--- index.js
+++ index.js
@@ -7,6 +7,4 @@ import App from './App';
 import {name as appName} from './app.json';
 import AuthScreen from './component/src/AuthScreen/AuthScreen'
 
-
-
-AppRegistry.registerComponent(appName, () =>AuthScreen);
+AppRegistry.registerComponent(appName, () => AuthScreen);
