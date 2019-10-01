9759e0633ab3753ec7d85ba6aac55d4336ee5729
dimesha <dimesha99kanchani@gmail.com>
2019-10-01 10:26:53 +0530

    added folder structure

--- index.js
+++ index.js
@@ -5,5 +5,8 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import AuthScreen from './component/src/AuthScreen/AuthScreen'
 
-AppRegistry.registerComponent(appName, () => App);
+
+
+AppRegistry.registerComponent(appName, () =>AuthScreen);
