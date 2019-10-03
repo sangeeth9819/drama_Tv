3ebf41df94100b244ba33cfdbd7fe270549c19b7
dimesha <dimesha99kanchani@gmail.com>
2019-10-03 16:08:47 +0530

    added Navigation

--- App.js
+++ App.js
@@ -5,110 +5,73 @@
  * @format
  * @flow
  */
+import React, { Component } from 'react';
+import {Navigator} from 'react-native'
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator, Header } from 'react-navigation-stack';
+import { View } from 'native-base';
+import Auth from './component/src/AuthScreen/AuthScreen';
+import Channel from './component/src/ChannelScreen/ChannelScreen';
+import Episode from './component/src/EpisodeScreen/EpisodeScreen';
+import Play from './component/src/PlayScreen/PlayScreen';
+import SideMenu from './component/src/SideMenuscreen/SideMenuScreen';
+import Teledrama from './component/src/TeledramaScreen/TeledramaScreen';
 
-// import React from 'react';
-// import {
-//   SafeAreaView,
-//   StyleSheet,
-//   ScrollView,
-//   View,
-//   Text,
-//   StatusBar,
-// } from 'react-native';
 
-// import {
-//   Header,
-//   LearnMoreLinks,
-//   Colors,
-//   DebugInstructions,
-//   ReloadInstructions,
-// } from 'react-native/Libraries/NewAppScreen';
 
-// const App: () => React$Node = () => {
-//   return (
-//     <>
-//       <StatusBar barStyle="dark-content" />
-//       <SafeAreaView>
-//         <ScrollView
-//           contentInsetAdjustmentBehavior="automatic"
-//           style={styles.scrollView}>
-//           <Header />
-//           {global.HermesInternal == null ? null : (
-//             <View style={styles.engine}>
-//               <Text style={styles.footer}>Engine: Hermes</Text>
-//             </View>
-//           )}
-//           <View style={styles.body}>
-//             <View style={styles.sectionContainer}>
-//               <Text style={styles.sectionTitle}>Step One</Text>
-//               <Text style={styles.sectionDescription}>
-//                 Edit <Text style={styles.highlight}>App.js</Text> to change this
-//                 screen and then come back to see your edits.
-//               </Text>
-//             </View>
-//             <View style={styles.sectionContainer}>
-//               <Text style={styles.sectionTitle}>See Your Changes</Text>
-//               <Text style={styles.sectionDescription}>
-//                 <ReloadInstructions />
-//               </Text>
-//             </View>
-//             <View style={styles.sectionContainer}>
-//               <Text style={styles.sectionTitle}>Debug</Text>
-//               <Text style={styles.sectionDescription}>
-//                 <DebugInstructions />
-//               </Text>
-//             </View>
-//             <View style={styles.sectionContainer}>
-//               <Text style={styles.sectionTitle}>Learn More</Text>
-//               <Text style={styles.sectionDescription}>
-//                 Read the docs to discover what to do next:
-//               </Text>
-//             </View>
-//             <LearnMoreLinks />
-//           </View>
-//         </ScrollView>
-//       </SafeAreaView>
-//     </>
-//   );
-// };
 
-// const styles = StyleSheet.create({
-//   scrollView: {
-//     backgroundColor: Colors.lighter,
-//   },
-//   engine: {
-//     position: 'absolute',
-//     right: 0,
-//   },
-//   body: {
-//     backgroundColor: Colors.white,
-//   },
-//   sectionContainer: {
-//     marginTop: 32,
-//     paddingHorizontal: 24,
-//   },
-//   sectionTitle: {
-//     fontSize: 24,
-//     fontWeight: '600',
-//     color: Colors.black,
-//   },
-//   sectionDescription: {
-//     marginTop: 8,
-//     fontSize: 18,
-//     fontWeight: '400',
-//     color: Colors.dark,
-//   },
-//   highlight: {
-//     fontWeight: '700',
-//   },
-//   footer: {
-//     color: Colors.dark,
-//     fontSize: 12,
-//     fontWeight: '600',
-//     padding: 4,
-//     paddingRight: 12,
-//     textAlign: 'right',
-//   },
-// });
 
-// export default App;
+
+const RootStack = createStackNavigator({
+    AuthScreen: {
+    screen: Auth,
+    navigationOptions: { header: null }
+
+  },
+  ChannelScreen: {
+    screen: Channel,
+    navigationOptions: { header: null }
+
+  },
+//   EpisodeScreen: {
+//     screen:Episode,
+//     navigationOptions: { header: null }
+//   },
+//   PlayScreen: {
+//     screen: Play,
+//     navigationOptions: { header: null }
+//   },
+//   SideMenuScreen: {
+//     screen: SideMenu,
+//     navigationOptions: { header: null }
+//   },
+//   TeledramaScreen: {
+//     screen: Teledrama,
+//     navigationOptions: { header: null }
+//   },
+
+},
+
+  {
+    initialRouteName: 'ChannelScreen'
+  },
+  {
+    headerMode: 'screen'
+  },
+);
+
+const AppContainer = createAppContainer(RootStack);
+
+export default class App extends Component {
+  render() {
+    return (
+      < View style={{ flex: 1 }}>
+
+        <AppContainer />
+        
+
+      </View>
+    )
+  }
+}
+ 
--- index.js
+++ index.js
@@ -8,4 +8,4 @@ import {name as appName} from './app.json';
 import AuthScreen from './component/src/AuthScreen/AuthScreen'
 import ChannelScreen from './component/src/ChannelScreen/ChannelScreen'
 
-AppRegistry.registerComponent(appName, () => ChannelScreen);
+AppRegistry.registerComponent(appName, () => App);
