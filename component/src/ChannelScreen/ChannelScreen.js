6feb84681c370229add2d72fef16559e1e32fe9a
NewTech2000 <nipunasachith2000@gmail.com>
2019-10-11 15:30:44 +0530

    channelTodaycomplete

--- component/src/ChannelScreen/ChannelScreen.js
+++ component/src/ChannelScreen/ChannelScreen.js
@@ -77,14 +77,14 @@ export default class Channel extends Component {
 
     render() {
         const items = [
-            { name: 'Jathika Rupawahini', code: '#ffffff', image: require('../../assest/nationalTv.png') },
-            { name: 'ITN', code: '#ffffff', image: require('../../assest/itnTv.jpg') },
+            { name: '   National Tv', code: '#ffffff', image: require('../../assest/nationalTv.png') },
+            { name: '         ITN', code: '#ffffff', image: require('../../assest/itnTv.jpg') },
             { name: "Swarna wahini", code: "#ffffff", image: require('../../assest/Swarnavahini_logo.png') },
-            { name: 'siyath Tv', code: '#ffffff', image: require('../../assest/siyathaTv.png') },
-            { name: 'Tv 1', code: '#ffffff', image: require('../../assest/tv1Tv.png') },
-            { name: 'Sirasa Tv', code: '#ffffff', image: require('../../assest/sirasaTv.jpg') },
+            { name: '    siyath Tv', code: '#ffffff', image: require('../../assest/siyathaTv.png') },
+            { name: '        Tv 1', code: '#ffffff', image: require('../../assest/tv1Tv.png') },
+            { name: '       Sirasa Tv', code: '#ffffff', image: require('../../assest/sirasaTv.jpg') },
             { name: 'Hiru Tv', code: '#ffffff', image: require('../../assest/hiruTv.jpg') },
-        
+
             { name: 'Tv Deran', code: '#ffffff', image: require('../../assest/deranaTv.png') },
 
         ];
@@ -92,7 +92,7 @@ export default class Channel extends Component {
         return (
 
             <Drawer
-            
+
                 side="left" ref={(ref) => { this.drawer = ref; }}
                 acceptPan={true}
                 panOpenMask={1}
@@ -106,50 +106,50 @@ export default class Channel extends Component {
                     main: { opacity: (1 - ratio) / 1 }
                 })}>
                 <StatusBar barStyle="dark-content" hidden={false} backgroundColor="white" translucent={true} />
-                <View style={{backgroundColor:'#f5f5f0'}}>
+                <View style={{ backgroundColor: '#f5f5f0' }}>
 
-                <View style={{ top: 10  }}  >
-                    <Header style={{ backgroundColor: 'white', borderRadius: 30, top: 28, height: 44 }}>
-                        <TouchableOpacity onPress={() => this.openDrawer()} style={{ right: 15, }}>
-                            <View style={{ justifyContent: 'center', alignItems: 'center', alignContent: 'center' }}>
-                                <View style={{ width: 50, height: 60, borderRadius: 20 }}>
-                                    <View style={{ justifyContent: 'center', alignItems: 'center', alignContent: 'center' }}>
-                                        <Image style={{ width: 25, height: 25, top: 10 }} source={require('../../assest/menu.png')} />
+                    <View style={{ top: 10 }}  >
+                        <Header style={{ backgroundColor: 'white', borderRadius: 30, top: 28, height: 44 }}>
+                            <TouchableOpacity onPress={() => this.openDrawer()} style={{ right: 15, }}>
+                                <View style={{ justifyContent: 'center', alignItems: 'center', alignContent: 'center' }}>
+                                    <View style={{ width: 50, height: 60, borderRadius: 20 }}>
+                                        <View style={{ justifyContent: 'center', alignItems: 'center', alignContent: 'center' }}>
+                                            <Image style={{ width: 25, height: 25, top: 10 }} source={require('../../assest/menu.png')} />
+                                        </View>
                                     </View>
                                 </View>
-                            </View>
-                        </TouchableOpacity>
+                            </TouchableOpacity>
 
-                        <TouchableOpacity>
-                            <View style={{ justifyContent: 'center', alignItems: 'center', alignContent: 'center', top: 5 }}>
-                                <View style={{ width: 250, height: 30, borderRadius: 15, backgroundColor: '#f5f5f0' }}>
+                            <TouchableOpacity>
+                                <View style={{ justifyContent: 'center', alignItems: 'center', alignContent: 'center', top: 5 }}>
+                                    <View style={{ width: 250, height: 30, borderRadius: 15, backgroundColor: '#f5f5f0' }}>
 
-                                    <TextInput
-                                        style={{ left: 10, height: 40, borderColor: 'white', borderWidth: 1, borderRadius: 10, borderColor: '#FAFAFA' }}
-                                        placeholder='Search here' />
+                                        <TextInput
+                                            style={{ left: 10, height: 40, borderColor: 'white', borderWidth: 1, borderRadius: 10, borderColor: '#FAFAFA' }}
+                                            placeholder='Search here' />
 
-                                </View>
-                            </View>
-                        </TouchableOpacity>
-
-                        <TouchableOpacity onPress={() => Alert.alert("search workinng")} style={{ right: 0, left: 5 }}>
-                            <View style={{ justifyContent: 'center', alignItems: 'center', alignContent: 'center' }}>
-                                <View style={{ width: 30, height: 50, borderRadius: 30 }}>
-                                    <View style={{ justifyContent: 'center', alignItems: 'center', alignContent: 'center' }}>
-                                        <Image style={{ width: 25, height: 25, top: 8 }} source={require('../../assest/search.png')} />
                                     </View>
                                 </View>
-                            </View>
-                        </TouchableOpacity>
+                            </TouchableOpacity>
 
-                    </Header>
+                            <TouchableOpacity onPress={() => Alert.alert("search workinng")} style={{ right: 0, left: 5 }}>
+                                <View style={{ justifyContent: 'center', alignItems: 'center', alignContent: 'center' }}>
+                                    <View style={{ width: 30, height: 50, borderRadius: 30 }}>
+                                        <View style={{ justifyContent: 'center', alignItems: 'center', alignContent: 'center' }}>
+                                            <Image style={{ width: 25, height: 25, top: 8 }} source={require('../../assest/search.png')} />
+                                        </View>
+                                    </View>
+                                </View>
+                            </TouchableOpacity>
 
-                </View>
-                {/* Body Content */}
-                <View>
+                        </Header>
+
+                    </View>
+                    {/* Body Content */}
+
                     <ScrollView>
-                        <View>
 
+                        <View>
                             <View>
 
 
@@ -162,9 +162,9 @@ export default class Channel extends Component {
 
                                             renderItem={({ item, index }) => (
                                                 <TouchableOpacity onPress={() => this.navigatechannel()}>
-                                                    <View style={{ borderRadius: 30,shadowColor:"Black",shadowOffset:5,shadowOpacity:50 }}>
+                                                    <View style={{}}>
                                                         <View style={[styles.itemContainer, { backgroundColor: item.code }]}>
-                                                            <Image style={{ width: 130, height: 130, top: 16, borderRadius: 10 }} source={item.image} />
+                                                            <Image style={{ width: 100, height: 100, top: 15, borderRadius: 10, left: 10 }} source={item.image} />
                                                             <Text style={styles.itemName}>{item.name}</Text>
                                                             {/* <Text style={styles.itemCode}>{item.code}</Text> */}
                                                         </View>
@@ -178,9 +178,9 @@ export default class Channel extends Component {
                         </View>
                     </ScrollView>
                 </View>
-                </View>
+
             </Drawer>
-          
+
         );
     }
 }
@@ -191,9 +191,20 @@ const styles = StyleSheet.create({
     },
     itemContainer: {
         justifyContent: 'flex-end',
-        borderRadius: 5,
         padding: 10,
-        height: 150,
+        height: 130,
+        width: 140,
+        borderRadius: 20,
+        shadowColor: "#0000",
+        shadowOffset: {
+            width: 0,
+            height: 3,
+        },
+        shadowOpacity: 0.27,
+        shadowRadius: 10.65,
+
+        elevation: 6,
+
     },
     itemName: {
         top: 10,
@@ -206,6 +217,7 @@ const styles = StyleSheet.create({
         fontWeight: '600',
         fontSize: 12,
         color: '#000000',
-     
+
+
     },
 });
